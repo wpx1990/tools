7d89ddc4c488e364e3f46fe97dfaf40c8c476a39
wupeixin@zhejianglab.com <wupeixin@zhejianglab.com>
2024-08-05 11:02:20 +0800

    修改addpause

--- images/add-pause/go-pause/go_code/main.go
+++ images/add-pause/go-pause/go_code/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-    for true {
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), ": it will sleep 10s ...")
-        time.Sleep(10 * time.Second)
-    }
-
-    return
+	for {
+		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), ": it will sleep 10s ...")
+		time.Sleep(10 * time.Second)
+	}
 }
